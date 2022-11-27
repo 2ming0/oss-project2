acbd26d095915cad196e14a01394d7421baa1422
2ming0 <betty5454@daum.net>
2022-11-27 17:36:26 +0900

    final

--- template.py
+++ template.py
@@ -2,30 +2,69 @@
 #https://github.com/2ming0/oss_project2
 
 import sys
+import os
 import pandas as pd
-
-df = pd.read_csv("default_credit_card.csv")
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score, recall_score, accuracy_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
 def load_dataset(dataset_path):
 	#To-Do: Implement this function
-	df = pd.read_csv("default_credit_card.csv")
-	print(df)
+	return pd.read_csv(dataset_path)
 
 def dataset_stat(dataset_df):	
 	#To-Do: Implement this function
-	return df.size
+    n_feats = dataset_df.shape[1] - 1
+    n_class0 = len(dataset_df.loc[dataset_df['target'] == 0])
+    n_class1 = len(dataset_df.loc[dataset_df['target'] == 1])
+    return n_feats, n_class0, n_class1
 
 def split_dataset(dataset_df, testset_size):
-	#To-Do: Implement this function
+    #To-Do: Implement this function
+    df_data = dataset_df.drop('target', axis=1)
+    df_target = dataset_df['target']
+    return train_test_split(df_data, df_target, test_size=testset_size)
 
 def decision_tree_train_test(x_train, x_test, y_train, y_test):
-	#To-Do: Implement this function
+    #To-Do: Implement this function
+	dt = DecisionTreeClassifier()
+	dt.fit(x_train, y_train)
+	acc = accuracy_score(y_test,dt.predict(x_test))
+	prec = precision_score(y_test,dt.predict(x_test))
+	recall = recall_score(y_test,dt.predict(x_test))
+	return acc, prec, recall
 
 def random_forest_train_test(x_train, x_test, y_train, y_test):
-	#To-Do: Implement this function
+    #To-Do: Implement this function
+    rf = RandomForestClassifier()
+    rf.fit(x_train, y_train)
+    pred = rf.predict(x_test)
+    
+    acc = accuracy_score(y_test, pred)
+    precision = precision_score(y_test, pred)
+    recall = recall_score(y_test, pred)
+    
+    return acc, precision, recall
 
 def svm_train_test(x_train, x_test, y_train, y_test):
 	#To-Do: Implement this function
+	pipe = make_pipeline(
+		StandardScaler(),
+		SVC()
+	)
+	pipe.fit(x_train, y_train)
+	
+	pred = pipe.predict(x_test)
+ 
+	acc = accuracy_score(y_test, pred)
+	prec = precision_score(y_test, pred)
+	recall = recall_score(y_test, pred)
+	
+	return (acc, prec, recall)
 
 def print_performances(acc, prec, recall):
 	#Do not modify this function!
